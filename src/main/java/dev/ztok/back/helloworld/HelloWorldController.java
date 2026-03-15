e5549787fe4ce26a9b13631e2dbcf3787f5c336e
48-iq <ivn25ov@mail.ru>
2026-03-15 20:07:58 +0400

    configuration dev environment

---
+++ src/main/java/dev/ztok/back/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package dev.ztok.back.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+  
+  @GetMapping("/")
+  public String helloWorld() {
+    return "<h1>Hello world!!!</h1>";
+  }
+}
---
+++ src/main/java/dev/ztok/back/security/SecurityConfig.java
@@ -0,0 +1,8 @@
+package dev.ztok.back.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+  
+}
