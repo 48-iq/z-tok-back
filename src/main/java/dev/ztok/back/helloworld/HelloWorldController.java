7bfa411e5062069a6500833e18686cef93a9d897
48-iq <ivn25ov@mail.ru>
2026-03-23 12:58:14 +0400

    build workflow

--- src/main/java/dev/ztok/back/helloworld/HelloWorldController.java
+++ src/main/java/dev/ztok/back/helloworld/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
-  
+
   @GetMapping("/")
   public String helloWorld() {
     return "<h1>Hello world!!!</h1>";
